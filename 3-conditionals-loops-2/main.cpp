b61a77d6d41813009abea12941cb94f959d3c030
Hajol <vadim.zhukvo.999@mail.ru>
2018-10-02 20:48:33 +0300

    correct

--- 3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/main.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 int main() {
 	double x1, EPS, dx, x2;
-	const int Max_Iter = 1000;
+	double arc1 = 0, arc2 = M_PI_2;
+	const int Max_Iter = 100;
 
 	cout << fixed;
+	cout << "x must be greater than 1\n";
 	cout << "Enter X start: ";
 	cin >> x1;
 	cout << "Enter x end: ";
@@ -20,7 +22,7 @@ int main() {
 	cout << "Enter Eps: ";
 	cin >> EPS;
 
-	if (dx > 0 && x2 > 1) {
+	if (dx > 0 && x2 > 1 && x1 > 1) {
 
 
 		cout << string(60, '-') << "\n|"
@@ -33,11 +35,9 @@ int main() {
 
 		for (double x = x1; x <= x2; x += dx) {
 
-			double arc1 = 0, arc2 = 0;
-
 			for (int n = 0; n < Max_Iter; n++) {
 
-				arc2 = (M_PI/2)+ pow(-1, n + 1) / ((2 * n + 1)*pow(x, 2 * n + 1));
+				arc2 +=pow (-1, n + 1) / ((2 * n + 1)*pow(x, 2 * n + 1));
 
 				if (abs(arc2 - arc1) < EPS) {
 					cout << "|" << setw(14) << x
@@ -48,18 +48,16 @@ int main() {
 
 
 				if (Max_Iter - n < 2) {
-					cout <<"small EPS";
+					cout << "small EPS";
 					return 2;
 				}
-				
+				arc1 = arc2;
 			}
 		}
 		cout << string(60, '-');
 	}
 
 	else {
-		cout << "Errror , invalid values";
+		cout << "Error , invalid values";
 	}
-
-	return 0;
 }
