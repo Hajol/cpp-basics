801de5347ba9f8ead1c80ca15fb52a9545475fc7
Hajol <vadim.zhukvo.999@mail.ru>
2018-10-10 22:37:30 +0300

    correct

--- 5-arrays-1d/main.cpp
+++ 5-arrays-1d/main.cpp
@@ -23,7 +23,7 @@ int main()
 		cin >> arr[i];
 	}
 	cout << "\n The minimum element index of the array is : " << FindArgMin(arr, n) << endl;
-	cout << "\n Sum of elements: " <<GetSum(arr, n) << endl;
+	cout << "\n Sum of the elements between the first and last negative: " <<GetSum(arr, n) << endl;
 	cout << "\n Converted array:" << endl;
 	TransformArray(arr, n);
 	for (int i = 0; i < n; i++) 
