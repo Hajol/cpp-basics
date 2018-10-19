ea370e233d08f36aedabbbbd881e366bc0d951ea
Hajol <vadim.zhukvo.999@mail.ru>
2018-10-19 17:02:25 +0300

    correct

--- 4-function/main.cpp
+++ 4-function/main.cpp
@@ -7,63 +7,72 @@
 using namespace std;
 
 void PrintTableHead() {
-	cout << string(60, '-') << "\n|"
-		<< setw(8) << "X" << setw(7)
-		<< "|" << setw(12) << "arctg(x)"
-		<< setw(3) << "|" << setw(12)
-		<< "atan(x)" << setw(3) << "|" << setw(12)
-		<< "Iterations" << setw(3) << "|\n"
-		<< string(60, '-') << endl;
+	cout << string(60, '-') << endl;
+	cout << "|" << setw(8) << "x" << setw(7) << "|";
+	cout << setw(12) << "arctg(x)" << setw(4) << "|";
+	cout << setw(11) << "atan(x)" << setw(4) << "|";
+	cout << setw(11) << "iterations" << setw(3) << "|\n";
+	cout << string(60, '-') << endl;
 }
 
-double arctg(double x, double eps, int &n, const int kMaxIters) {
+double ComputeSeries(double x, double eps, int &n, const int kMaxIters) {
 	double arc1, arc2 = M_PI_2;
 	for (n = 0; n < kMaxIters; n++) {
-		arc1 = pow(-1, n + 1) / ((2 * n + 1)*pow(x, 2 * n + 1));
+		arc1 = pow(-1, n + 1) / ((2 * n + 1) * pow(x, 2 * n + 1));
 		arc2 += arc1;
-
-		if (abs(arc1) < eps)
-			break;
+		if (abs(arc1) < eps) break;
 	}
 	return arc2;
 }
 
-void CreateRow(double x, double Eps) {
-	int n = 0;
-	const int kMaxIters = 100;
-	double y = arctg(x, Eps, n, kMaxIters);
-
-	cout << "|" << setw(14) << x << "|" << setw(14);
-	if (n == kMaxIters)
-		cout << "ERROR";
+void PrintTableRow(double x, double func, int n, const int kMaxIters) {
+	cout << "|" << setw(12) << x << setw(3) << "|" << setw(12);
+	if (n < kMaxIters)
+		cout << func << setw(4) << "|";
 	else
-		cout << y;
-	cout << "|" << setw(14) << atan(x)
-		<< "|" << setw(13) << n << "|\n";
+		cout << "   max iters   |";
+	cout << setw(11) << atan(x) << setw(4) << "|";
+	cout << setw(7) << n << setw(7) << "|\n";
 }
 
 int main() {
-	double x1, x2, dx, Eps;
+	const int kMaxIters = 1000;
 
-	cout << fixed;
-	cout << "X must be greater than 1\n";
-	cout << "Please, enter X star): ";
-	cin >> x1;
-	cout << "Please, enter X end: ";
-	cin >> x2;
-	cout << "Please, enter dX: ";
+	double xn, xk, dx, eps;
+	cout << "Please, enter xn (xn > 1): ";
+	cin >> xn;
+	cout << "Please, enter xk (xk >= xn): ";
+	cin >> xk;
+	cout << "Please, enter dx (dx > 0): ";
 	cin >> dx;
-	cout << "Please, enter eps: ";
-	cin >> Eps;
+	cout << "Please, enter eps (eps > 0): ";
+	cin >> eps;
 
-	if (dx > 0 && x2 > 1 && x1 > 1) {
+	if (dx <= 0) {
+		cout << "\nInvalid dx. Must be: dx > 0.\n";
+	}
+	else if (eps <= 0) {
+		cout << "\nInvalid eps. Must be: eps > 0.\n";
+	}
+	else if (xn <= 1) {
+		cout << "\nInvalid xn. Must be: xn > 1.\n";
+	}
+	else if (xn > xk) {
+		cout << "\nInvalid xk. Must be: xk >= xn.\n";
+	}
+	else {
 		PrintTableHead();
-		for (double x = x1; x <= x2; x += dx)
-			CreateRow(x, Eps);
+
+		cout << fixed;
+		cout.precision(6);
+
+		for (double x = xn; x <= xk; x += dx) {
+			int n;
+			double arctg = ComputeSeries(x, eps, n, kMaxIters);
+			PrintTableRow(x, arctg, n, kMaxIters);
+		}
 		cout << string(60, '-');
 	}
-	else
-		cout << "Error, Invalid values";
 
 	return 0;
 }
