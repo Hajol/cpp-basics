909cec034156edc6684372271d5ff7fe7b989772
Hajol <vadim.zhukvo.999@mail.ru>
2018-11-25 18:08:39 +0300

    correct

--- 6-arrays-2d/main.cpp
+++ 6-arrays-2d/main.cpp
@@ -9,85 +9,88 @@
 
 using namespace std;
 
-void TheSumOfElements(int a[Row][Col],int N[Col])
-{
+int SumInColumn(int arr[Row][Col], int column) {
+	int result = 0;
+	for (int i = 0; i < Row; i++)
+	{
+		result += arr[i][column];
+	}
+	return result;
+}
+
+void SwapColumns(int arr[Row][Col], int col_1, int col_2) {
+	for (int i = 0; i < Row; i++)
+	{
+		swap(arr[i][col_1], arr[i][col_2]);
+	}
+}
+
+void GetSubzeroModulo(int arr[Row][Col], int Subzero_Modulo[Col]) {
 	for (int i = 0; i < Col; i++)
 	{
-		N[i] = 0;
+		Subzero_Modulo[i] = 0;
 		for (int j = 0; j < Row; j++)
 		{
-			if (a[j][i] < 0 )
+			if (arr[j][i] < 0)
+				Subzero_Modulo[i] += abs(arr[j][i]);
+		}
+	}
+}
+
+void BubbleSort(int arr[Row][Col], int Subzero_Modulo[Col])
+{
+	for (int i = 0; i < Col - 1; i++)
+	{
+		for (int j = 0; j < Col - 1 - i; j++)
+		{
+			if (Subzero_Modulo[j] > Subzero_Modulo[j + 1])
 			{
-				N[i] += a[j][i];
-			}
-		}
-	} 
-}
-
-void SwapRows(int a[Row][Col], int r1, int r2) 
-{
-	int N[Col];
-	for (int i = 0; i < Col; i++)
-	{
-		N[i] = a[r1][i];
-	}
-	for (int i = 0; i < Col; i++)
-	{
-		a[r1][i] = a[r2][i];
-		a[r2][i] = N[i];
-	}
-}
-
-void SortingArray(int a[Row][Col]) 
-{
-	int N[Row] = { 0,0,0};
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Col; j++)
-		{
-			if ((a[i][j] > 0) && (a[i][j] % 2 != 0))
-				N[i] += abs(a[i][j]);
-		}
-	}
-
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Row - i - 1; j++)
-		{
-			if (N[j] > N[j + 1]) {
-				swap(N[j], N[j + 1]);
-				SwapRows(a, j, j + 1);
+				SwapColumns(arr, j, j + 1);
 			}
 		}
 	}
 }
 
-void PrintArray(int arr[Row][Col]) 
+void PrintSumColumns(int arr[Row][Col], int N[Col]) {
+	cout << "Sum In Columns with subzero elements: \n";
+	bool have = false;
+	for (int i = 0; i < Col; i++)
+	{
+		if (N[i] > 0) {
+			cout << i + 1 << ". sum = " << SumInColumn(arr, i) << endl;
+			have = true;
+		}
+	}
+	if (!have)
+		cout << "No columns with subzero elements!!!\n";
+}
+
+void PrintArray(int arr[Row][Col])
 {
 	cout << "Array: " << endl;
-	cout << string(21, '-') << endl;
-	for (int i = 0; i < Row; i++) 
+	cout << string(16, '-') << endl;
+	for (int i = 0; i < Row; i++)
 	{
 		cout << "|";
-		for (int j = 0; j < Col; j++) 
+		for (int j = 0; j < Col; j++)
 		{
 			cout << setw(3) << arr[i][j] << setw(2) << "|";
 		}
 		cout << endl;
 	}
-	cout << string(21, '-') << endl << endl;
+	cout << string(16, '-') << endl << endl;
 }
 
-void ReadArray(int arr[Row][Col], string name) 
+void ReadArray(int arr[Row][Col], string name)
 {
 	ifstream fin(name + ".txt");
 	if (!fin.is_open()) {
 		cout << "can't open file" << endl;
 		return;
 	}
-	for (int i = 0; i < Row; i++) 
+	for (int i = 0; i < Row; i++)
 	{
-		for (int j = 0; j < Col; j++) 
+		for (int j = 0; j < Col; j++)
 		{
 			fin >> arr[i][j];
 		}
@@ -95,21 +98,19 @@ void ReadArray(int arr[Row][Col], string name)
 	fin.close();
 }
 
-int main() 
+int main()
 {
 	int arr[Row][Col];
 	ReadArray(arr, "file");
 	PrintArray(arr);
-	SortingArray(arr);
-	PrintArray(arr);
-	cout << "Sum elements: " << endl;
-	int N[Col];
-	TheSumOfElements(arr, N);
 
-	for (int i = 0; i < Col; i++)
-	{
-		cout << i+1 << ". " << N[i] << endl;
-	}
-	cout << endl << endl;
+	cout << "Sorted array: \n\n";
+	int N[Col];
+	GetSubzeroModulo(arr, N);
+	BubbleSort(arr, N);
+	PrintArray(arr);
+
+	PrintSumColumns(arr, N);
+
 	return 0;
 }
